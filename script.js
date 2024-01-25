60769a16df73a303a9fe128c7ef75c9554cc181d
alexisgonzalez11 <156466804+alexisgonzalez11@users.noreply.github.com>
2024-01-25 11:26:27 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Ejemplo: Validar un correo electrónico
+function validarCorreoElectronico(correo) {
+    const patronCorreo = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return patronCorreo.test(correo);
+  }
+  
