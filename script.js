939e31baf4c51768833b2b49d7474e691adf9639
alexisgonzalez11 <156466804+alexisgonzalez11@users.noreply.github.com>
2024-01-25 11:39:45 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,13 @@ function validarCorreoElectronico(correo) {
     const patronCorreo = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     return patronCorreo.test(correo);
   }
+  function validateForm() {
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+
+    // Verificar si la contraseña cumple con ciertos criterios (puedes ajustarlos según tus necesidades)
+    if (password.length < 8) {
+      document.getElementById("passwordError").innerText = "La contraseña debe tener al menos 8 caracteres.";
+      return false;
+        
   
